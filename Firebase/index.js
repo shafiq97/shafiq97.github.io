2bc2a11e94eb0f82359f249b2bdd7ef73be8e31a
shafiq <shafiq_razman@yahoo.com>
2019-12-29 07:36:20 +0800

    get room id from server

--- Firebase/index.js
+++ Firebase/index.js
@@ -20,8 +20,91 @@ firebase.firestore().settings({
 //enable offline database
 firebase.firestore().enablePersistence();
 
+function getRoomID() {
+
+  var userPass = document.getElementById("password_field").value;
+  var userEmail = document.getElementById("email_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+  });
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      user = firebase.auth().currentUser;
+      var uid = user.uid;
+      var cemail;
+
+      var db = firebase.firestore();
+      var userRef = db.collection("users").doc(uid);
+
+      userRef.get().then(function(doc) {
+          if (doc.exists) {
+              if(doc.data().Role == "Interviewer") {
+                document.getElementById("room_id").innerHTML = "Enter your candidate email";
+                var ridi = document.createElement("INPUT");
+                ridi.setAttribute("id","ridi");
+                document.getElementById("room_id").appendChild(ridi);
+                document.getElementById("room_id").disabled = true;
+
+
+                $("#ridi").on('keyup', function (e) {
+                    if (e.keyCode === 13) {
+                        // Do something
+                        cemail = document.getElementById("ridi").value;
+                        console.log(cemail);
+                        var userRef2 = db.collection("users");
+
+                        var query = userRef2.where("Email", "==", cemail)
+                          .get()
+                          .then(function(querySnapshot) {
+                              querySnapshot.forEach(function(doc) {
+                                  // doc.data() is never undefined for query doc snapshots
+                                  console.log(doc.id, " => ", doc.data().RoomID);
+                                  var ridi2 = document.createElement("INPUT");
+                                  ridi2.setAttribute("id","ridi2");
+                                  document.getElementById("room_id").appendChild(ridi2);
+                                  ridi2.value = doc.data().RoomID;
+                                  document.getElementById("room_id").disabled = true;
+                              });
+                          })
+                          .catch(function(error) {
+                              console.log("Error getting documents: ", error);
+                          });
+                    }
+                });
+              } 
+              else 
+              {
+                var rid = document.createElement("INPUT");
+                rid.value = doc.data().RoomID;
+                document.getElementById("room_id").appendChild(rid);
+                document.getElementById("room_id").disabled = true;
+                //document.getElementById("room_id").value = doc.data().RoomID;
+              }
+          } else {
+              // doc.data() will be undefined in this case
+              console.log("No such document!");
+          }
+      }).catch(function(error) {
+          console.log("Error getting document:", error);
+      });
+    } 
+    else {
+      // No user is signed in.
+      alert("Room ID will automatically filled based on your email");
+    }
+  });
+}
+
 
 function getUserRole(uid) {
+
   var db = firebase.firestore();
   var userRef = db.collection("users").doc(uid);
   userRef.get().then(function(doc) {
@@ -31,7 +114,6 @@ function getUserRole(uid) {
             window.location.href = "Interviewer.html";
           } else {
             document.getElementById("room_id").value = doc.data().RoomID;
-            alert(doc.data().RoomID);
             window.location.href = "Interviewee.html";
           }
       } else {
@@ -81,25 +163,44 @@ function login() {
 
     window.alert("Error : " + errorMessage);
   });
-}
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    user = firebase.auth().currentUser;
-    var uid = user.uid;
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      user = firebase.auth().currentUser;
+      var uid = user.uid;
 
-    if(user != null) {
-      this.userEmail = document.getElementById("email_field").value;
-       getUserRole(uid);
+      if(user != null) {
+        this.userEmail = document.getElementById("email_field").value;
+        getUserRole(uid);
+      }
       
+    } 
+    else {
+      // No user is signed in.
+      document.getElementById("login_div").style.display = "block";
     }
-  } 
-  else {
-    // No user is signed in.
-    document.getElementById("login_div").style.display = "block";
-  }
-});
+  });
+
+}
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      user = firebase.auth().currentUser;
+      var uid = user.uid;
+
+      if(user != null) {
+        this.userEmail = document.getElementById("email_field").value;
+      }
+      
+    } 
+    else {
+      // No user is signed in.
+      document.getElementById("login_div").style.display = "block";
+    }
+  });
+
+
 
 function logout() {
   firebase.auth().signOut();
